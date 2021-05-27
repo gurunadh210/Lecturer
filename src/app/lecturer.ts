519ef2e163f0b26d6faf80ccc371764bb2f53d67
gurunadh210! <chenngurunadh@gmail.com>
2021-05-27 17:45:40 +0530

    first commit

---
+++ src/app/lecturer.ts
@@ -0,0 +1,7 @@
+export interface Lecturer {     //this is a lecturer interface class
+    id: number;                 //this is a datatypes  
+    name: string;
+    email: string;
+    phonenumber: number;
+
+}
